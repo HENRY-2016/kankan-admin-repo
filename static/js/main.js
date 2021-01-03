4366ba3b5279cc2c039a5eab68fe756269ea7e2f
Henry-2016 <jacob2016henry@gmail.com>
2021-01-04 00:26:18 +0300

    New Bigtime Admin Apk Uploads

--- static/js/main.js
+++ static/js/main.js
@@ -1,32 +1,53 @@
 
 // 192.168.43.23
 
-function LoadUsingGitHub ()
-{window.location='https://henry-2016.github.io/kankan-admin-server-repo/'}
 
-function LoadUsingLapTop ()
-{window.location="http://192.168.43.23:80/kankan-admin-server-repo/index.html"}
-
-function LoadUsingRaspberry ()
-{window.location="http://192.168.43.140:80/kankan-admin-server-repo/index.html"}
-
-
-// Esther
-function LoadEstherUsingGitHub ()
-{window.location='https://henry-2016.github.io/esther-admin-server-repo/'}
-
-function LoadEstherUsingLapTop ()
-{window.location="http://192.168.43.23:80/esther-admin-server-repo/index.html"}
-
-function LoadEstherUsingRaspberry ()
-{window.location="http://192.168.43.140:80/esther-admin-server-repo/index.html"}
-
-
-function Create_Offline_Label ()
+function CreateCustomAlertBox()
 {
-    let offline_label_div = document.getElementById("offline-label-div");
-    let label = document.createElement("label");
-    label.innerHTML = "Your Are Offline <br> Please <br>Connect To Network"
-    label.className = "Offline-Label"
-    offline_label_div.appendChild(label)
+    this.render = function(dialog){
+        let winW = window.innerWidth;
+        let winH = window.innerHeight;
+        let AlertBoxDisplayArea = document.getElementById('alert-box-display-area');
+        let HtmlAlertBox = document.getElementById('alert-box');
+        AlertBoxDisplayArea.style.display = "block";
+        AlertBoxDisplayArea.style.height = winH+"px";
+        HtmlAlertBox.style.left = (winW/2) - (550 * .5)+"px";
+        HtmlAlertBox.style.top = "100px";
+        HtmlAlertBox.style.display = "block";
+        document.getElementById('alert-box-header').innerHTML = "Network Or Server Error ";
+        document.getElementById('alert-box-body').innerHTML = dialog;
+        document.getElementById('alert-box-footer').innerHTML = '<button class="ok-alert-btn" onclick="Alert.ok()">OK</button>';
+    }
+    this.ok = function()
+        {
+            document.getElementById('alert-box').style.display = "none";
+            document.getElementById('alert-box-display-area').style.display = "none";
+        }
 }
+var Alert = new CreateCustomAlertBox();
+
+function ShowAlertBox (){Alert.render('<center>Please turn on <br> your network <br>and <br> Try Again</center>')}
+
+
+// Adonoi
+function LoadAdonoiUsingGitHub ()
+{
+    if(!navigator.onLine)
+    {
+        CreateCustomAlertBox ()
+    }
+    
+    if(navigator.onLine)
+    {window.location='https://henry-2016.github.io/bigtime-admin-server-repo/'}
+    
+}
+
+function LoadAdonoiUsingRaspberry ()
+
+{
+    if(!navigator.onLine)
+        {CreateCustomAlertBox ()}
+    if(navigator.onLine)
+        {window.location="http://192.168.43.140:80/bigtime-admin-server-repo/index.html"}
+}
+
